9cd1950232fc059f9d8168216a9200631ee26518
Robert Jardine <robertjardine@gmail.com>
2017-06-28 21:57:58 -0500

    Update "Hint" functionality. Changed Recursion+Binary Search method to implementing a letter frequency based search to improve performance drastically.

--- app/src/main/java/gaiginja/wordz/ChooseLetter.java
+++ app/src/main/java/gaiginja/wordz/ChooseLetter.java
@@ -38,9 +38,9 @@ import java.util.Set;
 public class ChooseLetter extends AppCompatActivity {
     EditText userInput;
     TextView score;
-    private ArrayList<String> dict = new ArrayList<>();
+    private List<String> dictionary = new ArrayList<>();
     private Set<String> possibleWords = new HashSet<>();
-    private Map<Character, Integer> charMap = new HashMap<>();
+    private Map<Character, Integer> pointsPerChar = new HashMap<>();
     private ArrayList<Button> buttonArray = new ArrayList<>();
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     private static final String LETTERFREQ = "aaaaaaaaabbccddddeeeeeeeeeeeeffggghhiiiiiiiiijkllllmmnnnnnnooooooooppqrrrrrrssssttttttuuuuvvwwxyyz";
@@ -65,12 +65,13 @@ public class ChooseLetter extends AppCompatActivity {
         submitButton.setBackgroundColor(Color.RED);
 
         //Try-with-resouces not supported by older versions of Android
+        //Initialize dictionary
         try {
             BufferedReader fileRead = new BufferedReader(new InputStreamReader(getAssets().open(FILENAME)));
             String line = fileRead.readLine();
             //Initialize Words in Dictionary
             while(line != null){
-                dict.add(line);
+                dictionary.add(line);
                 line = fileRead.readLine();
             }
             fileRead.close();
@@ -83,7 +84,7 @@ public class ChooseLetter extends AppCompatActivity {
         }
 
         //Initilize Button objects
-        //Keep track of buttons for later use by putting them in an ArrayList
+        //Keep track of buttons for later use by putting them into a List
         letter1 = (Button)findViewById(R.id.letter1);
         buttonArray.add(letter1);
         letter2 = (Button)findViewById(R.id.letter2);
@@ -136,7 +137,7 @@ public class ChooseLetter extends AppCompatActivity {
 
     /**
      * Method called when a letter from the pool is clicked on.
-     * Image will become transparent and associated char will be added to answer.
+     * Image will become transparent and associated char will be added to the answer.
      * @param v - Holds details of the object that was clicked.
      */
     public void letterClicked(View v) {
@@ -152,13 +153,14 @@ public class ChooseLetter extends AppCompatActivity {
             submitButton.setBackgroundColor(Color.GREEN);
         }
 
-        // Remove letter Once it ss chosen
+        // Remove letter once it is chosen
         buttonClicked.setVisibility(View.GONE);
     }
 
     /**
      * Check if the entered input is valid.
      * If it is, then award points. Else, put the letters back.
+     * @param v - Holds details of the object that was clicked.
      */
     public void submitClicked(View v) {
         String submitInput = String.format("%s", userInput.getText());
@@ -172,7 +174,7 @@ public class ChooseLetter extends AppCompatActivity {
             Log.d("submitClicked", "newScore=" + score.getText());
         } else {
             for(int i=0; i<submitInput.length(); i++){
-                Log.d("submitClicked", "message=revertCharacter BEGIN LOOP");
+                Log.d("submitClicked", "message=revertCharacterBegin");
                 for(Button button : buttonArray) {
                     if( (button.getText().equals(String.format("%s", submitInput.charAt(i))))
                         && (button.getVisibility() != View.VISIBLE)){
@@ -192,7 +194,7 @@ public class ChooseLetter extends AppCompatActivity {
      * Reset all variables for a new game.
      * @param v - Holds the properties of the restart button
      */
-    public void restart(View v){
+    public void restart(View v) {
         initializeButtonText(buttonArray);
         for(Button button : buttonArray) {
             if(button.getVisibility() != View.VISIBLE){
@@ -205,19 +207,121 @@ public class ChooseLetter extends AppCompatActivity {
     }
 
     /**
+     * Initialize the letters that will be displayed on the buttons in the pool of letters
+     * @param buttonArray - array of button objects that will hold a single letter on the screen
+     */
+    private void initializeButtonText(ArrayList<Button> buttonArray) {
+        for(Button button : buttonArray){
+            Random randNum = new Random();
+            //Add random letter from LETTERFREQ to current button
+            int stringIndex = randNum.nextInt(LETTERFREQ.length()-1);
+            button.setText(String.format("%s", LETTERFREQ.charAt(stringIndex)));
+        }
+    }
+
+    /**
+     * Assign a point value to all letters.
+     * This information is stored in the Hashmap variable "pointsPerChar"
+     * Will be used when adding points after a successful word is entered.
+     */
+    private void initializeLetterValues() {
+        for(int i=0; i<ALPHABET.length(); i++){
+            switch(ALPHABET.charAt(i)) {
+                case 'a':
+                    pointsPerChar.put('a', 1);
+                case 'b':
+                    pointsPerChar.put('b', 3);
+                case 'c':
+                    pointsPerChar.put('c', 3);
+                case 'd':
+                    pointsPerChar.put('d', 2);
+                case 'e':
+                    pointsPerChar.put('e', 1);
+                case 'f':
+                    pointsPerChar.put('f', 4);
+                case 'g':
+                    pointsPerChar.put('g', 2);
+                case 'h':
+                    pointsPerChar.put('h', 4);
+                case 'i':
+                    pointsPerChar.put('i', 1);
+                case 'j':
+                    pointsPerChar.put('j', 8);
+                case 'k':
+                    pointsPerChar.put('k', 5);
+                case 'l':
+                    pointsPerChar.put('l', 1);
+                case 'm':
+                    pointsPerChar.put('m', 3);
+                case 'n':
+                    pointsPerChar.put('n', 1);
+                case 'o':
+                    pointsPerChar.put('o', 1);
+                case 'p':
+                    pointsPerChar.put('p', 3);
+                case 'q':
+                    pointsPerChar.put('q', 10);
+                case 'r':
+                    pointsPerChar.put('r', 1);
+                case 's':
+                    pointsPerChar.put('s', 1);
+                case 't':
+                    pointsPerChar.put('t', 1);
+                case 'u':
+                    pointsPerChar.put('u', 1);
+                case 'v':
+                    pointsPerChar.put('v', 4);
+                case 'w':
+                    pointsPerChar.put('w', 4);
+                case 'x':
+                    pointsPerChar.put('x', 8);
+                case 'y':
+                    pointsPerChar.put('y', 4);
+                case 'z':
+                    pointsPerChar.put('z', 10);
+            }
+        }
+    }
+
+    /**
+     * Check if the entered word is a valid English word
+     * @param userInput - Word entered by the user
+     * @return - If the word is valid.
+     */
+    private boolean inDict(String userInput) {
+        return dictionary.contains(userInput);
+    }
+
+    /**
+     * Use the pre-determined points per letter to
+     * calculate the value of the new word entered.
+     * @param input - String to analyze.
+     * @return - Point value of the word.
+     */
+    private int getPoints(String input) {
+        int result = 0;
+        for(int i=0; i<input.length(); i++){
+            int letterValue = pointsPerChar.get(input.charAt(i));
+            result += letterValue;
+        }
+        Log.d("getPoints", "totalPointsAdded=" + result);
+        return result;
+    }
+
+    /**
      * Displays all valid words that can be
      * created with the remaining letters.
      * @param v - Holds properties of the Hint button.
      */
     public void showHint(View v) {
-        String remainingLetters = "";
+        StringBuilder remainingLetters = new StringBuilder("");
         for(Button button : buttonArray){
             if(button.getVisibility() == View.VISIBLE){
-                remainingLetters = remainingLetters + button.getText();
+                remainingLetters.append(button.getText());
             }
         }
         Log.d("showHint", "remainingLetters=" + remainingLetters);
-        getAllValidWords(remainingLetters, "");
+        getAllValidWords(remainingLetters.toString());
         List<String> possibleWordsList = new ArrayList<>(possibleWords);
         Collections.sort(possibleWordsList);
         Log.d("showHint", "possibleWords=" + possibleWordsList);
@@ -248,169 +352,52 @@ public class ChooseLetter extends AppCompatActivity {
     }
 
     /**
-     * Initialize the letters that will be displayed on the buttons in the pool of letters
-     * @param buttonArray - array of button objects that will hold a single letter on the screen
+     * Populate possibleWords with all valid English Words
+     * that can be formed with the given letters.
+     * @param letterPool - Current letters that the user can choose from.
      */
-    private void initializeButtonText(ArrayList<Button> buttonArray){
-        for(Button button : buttonArray){
-            Random randNum = new Random();
-            //Add random letter from LETTERFREQ to current button
-            int stringIndex = randNum.nextInt(LETTERFREQ.length()-1);
-            button.setText(String.format("%s", LETTERFREQ.charAt(stringIndex)));
-        }
-    }
-
-    /**
-     * Assign a point value to all letters.
-     * This information is stored in the Hashmap variable "charMap"
-     * Will be used when adding points after a successful word is entered.
-     */
-    private void initializeLetterValues() {
-        for(int i=0; i<ALPHABET.length(); i++){
-            switch(ALPHABET.charAt(i)) {
-                case 'a':
-                    charMap.put('a', 1);
-                case 'b':
-                    charMap.put('b', 3);
-                case 'c':
-                    charMap.put('c', 3);
-                case 'd':
-                    charMap.put('d', 2);
-                case 'e':
-                    charMap.put('e', 1);
-                case 'f':
-                    charMap.put('f', 4);
-                case 'g':
-                    charMap.put('g', 2);
-                case 'h':
-                    charMap.put('h', 4);
-                case 'i':
-                    charMap.put('i', 1);
-                case 'j':
-                    charMap.put('j', 8);
-                case 'k':
-                    charMap.put('k', 5);
-                case 'l':
-                    charMap.put('l', 1);
-                case 'm':
-                    charMap.put('m', 3);
-                case 'n':
-                    charMap.put('n', 1);
-                case 'o':
-                    charMap.put('o', 1);
-                case 'p':
-                    charMap.put('p', 3);
-                case 'q':
-                    charMap.put('q', 10);
-                case 'r':
-                    charMap.put('r', 1);
-                case 's':
-                    charMap.put('s', 1);
-                case 't':
-                    charMap.put('t', 1);
-                case 'u':
-                    charMap.put('u', 1);
-                case 'v':
-                    charMap.put('v', 4);
-                case 'w':
-                    charMap.put('w', 4);
-                case 'x':
-                    charMap.put('x', 8);
-                case 'y':
-                    charMap.put('y', 4);
-                case 'z':
-                    charMap.put('z', 10);
-            }
-        }
-    }
-
-    /**
-     * Check if the entered word is a valid English word
-     * @param userInput - Word entered by the user
-     * @return - If the word is valid.
-     */
-    private boolean inDict(String userInput){
-        return dict.contains(userInput);
-    }
-
-    /**
-     * Binary Search of the dictionary for a certain string.
-     * Used to improve the execution time of the Hint search.
-     * @param word - Current String being checked
-     * @return - boolean: Whether a prefix is found in the dictionary.
-     */
-    private boolean binarySearch(String word){
-        int max = dict.size() - 1;
-        int min = 0;
-        int currIndex;
-        boolean result = false;
-        while(min <= max) {
-            currIndex = (min + max) / 2;
-            if (dict.get(currIndex).startsWith(word)) {
-                result = true;
-                break;
-            } else if (dict.get(currIndex).compareTo(word) < 0) {
-                min = currIndex + 1;
-            } else if(dict.get(currIndex).compareTo(word) > 0){
-                max = currIndex - 1;
-            } else {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Use the pre-determined points per letter to
-     * calculate the value of the new word entered.
-     * @param input - String to analyze.
-     * @return - Point value of the word.
-     */
-    private int getPoints(String input){
-        int result = 0;
-        for(int i=0; i<input.length(); i++){
-            int letterValue = charMap.get(input.charAt(i));
-            result += letterValue;
-        }
-        Log.d("getPoints", "totalPointsAdded=" + result);
-        return result;
-    }
-
-    private void getAllValidWords(String letterPool, String currWord) {
+    private void getAllValidWords(String letterPool) {
         //Add to possibleWords when valid word
-        if (letterPool.equals("")) {
-           // No action to be done.
-        } else if(currWord.equals("")){
-            //Will run only the initial time the method is called.
-            for (int i = 0; i < letterPool.length(); i++) {
-                //Get the individual letters that will become the first letter of a word
-                String curr = letterPool.substring(i, i+1);
-                //Delete the single letter from letterPool
-                String newLetterPool = (letterPool.substring(0, i) + letterPool.substring(i+1));
-                if(inDict(curr)){
-                    possibleWords.add(curr);
-                }
-                boolean prefixInDic = binarySearch(curr);
-                if(prefixInDic){
-                    //If the prefix isn't found, don't continue this recursive path.
-                    getAllValidWords(newLetterPool, curr);
-                }
-            }
-        } else {
-            //Every time we add a letter to currWord, delete from letterPool
-            for(int i=0; i<letterPool.length(); i++){
-                String curr = currWord + letterPool.substring(i, i+1);
-                String newLetterPool = (letterPool.substring(0, i) + letterPool.substring(i+1));
-                if(inDict(curr)){
-                    possibleWords.add(curr);
-                }
-                boolean prefixInDic = binarySearch(curr);
-                if(prefixInDic){
-                    //If the prefix isn't found, don't continue this recursive path.
-                    getAllValidWords(newLetterPool, curr);
-                }
+        int[] letterPoolFreq = getLetterFreq(letterPool);
+        for(String word : dictionary) {
+            int[] dictWordFreq = getLetterFreq(word);
+            if(checkLetterFrequency(letterPoolFreq, dictWordFreq)){
+                possibleWords.add(word);
             }
         }
+    }
+
+    /**
+     * Compares the frequency of letters in two Strings
+     * @param letterPoolFreq - Stores the letter frequency in the String letterFrequency.
+     * @param dictWordFreq - Stores the letter frequency of the current dictionary word.
+     * @return - if two parameter's letter frequencies match.
+     */
+    private boolean checkLetterFrequency(int[] letterPoolFreq, int[] dictWordFreq) {
+        for(int i=0; i<26; i++){
+            if(letterPoolFreq[i] < dictWordFreq[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Records the frequency of each letter
+     * that appears in a word.
+     * @param word - Analyzed for letter frequency.
+     * @return - int array with the letter frequencies.
+     */
+    private int[] getLetterFreq(String word) {
+        int[] letterFreq = new int[26];
+        for(int i=0; i<word.length(); i++){
+            //Ensure the character is a letter
+            char c = word.charAt(i);
+            if(((c - 'a') >= 0) && ((c - 'a') < 26)){
+                //Increment the frequency of the current letter
+                letterFreq[c - 'a']++;
+            }
+        }
+        return letterFreq;
     }
 }
